75226283433ffc2186e21b63afe644c867bd4df4
Raul Lopez <zepol@bellsouth.net>
2018-10-23 11:38:46 -0400

    Initial Commit

---
+++ AllMyList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllMyList
+//
+//  Created by Raul Lopez on 10/23/18.
+//  Copyright © 2018 Raul Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
