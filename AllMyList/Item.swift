3821ea4a25546358db64795b9455591a31a62363
Raul Lopez <zepol@bellsouth.net>
2018-10-24 19:13:04 -0400

    converted to mvc design

---
+++ AllMyList/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  AllMyList
+//
+//  Created by Raul Lopez on 10/24/18.
+//  Copyright © 2018 Raul Lopez. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
